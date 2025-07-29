4467aa06d32160ee89b7339c0e531b589491ce7c
ALEXRN13K <85494509+ALEXRN13K@users.noreply.github.com>
2025-07-29 22:32:47 +0200

    updated the code a bit

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+signal cave_change_to_black
 
 func _ready() -> void:
 	$ColorRect/AnimationPlayer.play("fade_to_normal")
+	
+	
+	
+func _on_cave_change_to_black() -> void:
+	$ColorRect/AnimationPlayer.play("fade_to_black")
